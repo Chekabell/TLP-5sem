dbd3022e86f5422731097ad8255d6685d88a8043
Chekabell <nikita.toporischev@mail.ru>
2024-10-06 21:20:49 +0500

    It's work

--- TLP-5sem.cpp
+++ TLP-5sem.cpp
@@ -1,31 +1,30 @@
-#include <iostream>
 #include <fstream>
 
-enum States { Default, Slash, Comment, Star };
+enum class States : std::uint8_t { Default, Slash, Comment, Star };
 
 int main() {
 	std::fstream fs;
 	std::ofstream out;
-	char tmp;
 	States state = States::Default;
 
 	fs.open("test.cpp");
-	out.open("outtest.cpp", std::ios::out);
+	out.open("out_test.cpp", std::ios::out);
 
 	if (fs.is_open() && out.is_open()) {
+		char tmp;
 		while (!fs.eof()) {
 			fs.get(tmp);
 			switch (state) {
 			case States::Default:
 				if (tmp == '/') state = States::Slash;
-				else out << tmp;
+				else out.put(tmp);
 				break;
 			case States::Slash:
-				if (tmp == '/') out << '/';
+				if (tmp == '/') out.put('/');
 				else if (tmp == '*') state = States::Comment;
 				else {
-					out << '/';
-					out << tmp;
+					out.put('/');
+					out.put(tmp);
 					state = States::Default;
 				}
 				break;
@@ -34,7 +33,7 @@ int main() {
 				break;
 			case States::Star:
 				if (tmp == '/') state = States::Default;
-				else if (tmp == '*') state = state;
+				else if (tmp == '*') state = States::Star;
 				else state = States::Comment;
 				break;
 			}
